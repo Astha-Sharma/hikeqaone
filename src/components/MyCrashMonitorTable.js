c5cba2929b3d658557e62edd116fbb8cd0a1a9ea
Vivek Vasvani <vivekvasvani@Viveks-MacBook-Pro.local>
2018-05-01 23:16:36 +0530

    area and assignee mapper

--- src/components/MyCrashMonitorTable.js
+++ src/components/MyCrashMonitorTable.js
@@ -3,6 +3,136 @@ import Table from './DefaultTable'
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 import Tooltip from 'rc-tooltip';
 
+var formData = {
+    issueId: {
+        "label": "Issue Id",
+        "type": "MyInput",
+        "value": "",
+        "placeholder":"No Space Allowed",
+        "mandatory": true,
+        "multiSelect": false,
+        "readOnly" : true
+    },
+    projectKey: {
+        "label": "Project Key",
+        "type": "MyInput",
+        "placeholder":"Project Key",
+        "value": "",
+        "mandatory": true,
+        "multiSelect": false
+    },
+    summary: {
+        "label": "Summary",
+        "type": "MyTextArea",
+        "value": "",
+        "mandatory": true,
+        "multiSelect": false,
+        "placeholder": "Add summary"
+    },
+    priority: {
+        "label": "Priority",
+        "type": "MyReactSelect",
+        "value": "",
+        "options": ['1 - Blocker', '2 - Critical', '3 - Major', '4 - Normal', '5 - Minor'],
+        "mandatory": true,
+        "multi": false,
+        "selectType": "Creatable",
+        "placeholder": "Priority"
+    },
+    assignee: {
+        "label": "Assignee",
+        "type": "MyInput",
+        "placeholder":"Assignee",
+        "value": "",
+        "mandatory": true
+    },
+    platform: {
+        "label": "Platform",
+        "type": "MyReactSelect",
+        "value": "",
+        "options": ['Android', 'iOS', 'Android-Web', 'iOS-Web'],
+        "mandatory": true,
+        "multi": false,
+        "selectType": "Creatable",
+        "placeholder": "Platform"
+    },
+    fixVersion: {
+        "label": "Fix Version",
+        "type": "MyInput",
+        "value": "",
+        "mandatory": true,
+        "multi": false,
+        "selectType": "Creatable",
+        "placeholder": "Fix Version"
+    },
+    description: {
+        "label": "Description",
+        "type": "MyTextArea",
+        "value": "",
+        "mandatory": true,
+        "multiSelect": false,
+        "placeholder": "Description"
+    },
+    marketBehaviour: {
+        "label": "Market Behaviour",
+        "type": "MyReactSelect",
+        "value": "",
+        "options": ['New', 'Already in Market'],
+        "mandatory": true,
+        "multi": false,
+        "selectType": "Creatable",
+        "placeholder": "Market Behaviour"
+    },
+    reproRate: {
+        "label": "Repro Rate",
+        "type": "MyReactSelect",
+        "value": "",
+        "options": ['100%', 'Happening Frequently (3/5)', 'Happening Rarely (<3/5)', 'Observed Once'],
+        "mandatory": true,
+        "multi": false,
+        "selectType": "Creatable",
+        "placeholder": "Repro Rate"
+    },
+    label: {
+        "label": "Label (Option)",
+        "type": "MyInput",
+        "value": "",
+        "mandatory": false,
+        "multi": false,
+        "selectType": "Creatable",
+        "placeholder": "Label"
+    },
+    cancel: {
+        "label": "Cancel" ,
+        "type": "MyButton",
+        "theme": "red",
+        "className": "cceButton pull-left",
+        "disabled": false
+    },
+    submit: {
+        "label": "Create Jira" ,
+        "type": "MyButton",
+        "theme": "green",
+        "className": "cceButton pull-left",
+        "buttonType": "submit",
+        "disabled": false
+    }
+}
+
+const assignee_mapper = {'BCN': 'radhakrishna', 'CD': 'sk', 'CH': 'radhakrishna', 'SCS': 'radhakrishna',
+    'APPX': 'arpit', 'CAM': 'sourav', 'STO': 'vaibhavt', 'PP': 'arpit', 'ON': 'arpit', 'US': 'arpit',
+    'HP': 'abhinav', 'HL': 'lakshay', 'BR': 'prateekd', 'FRIENDS': 'vaibhavt', 'COM': 'arpit', 'NOTIF': 'arpit'}
+
+const area_mapper = {'Chat': 'BCN', 'DevX': 'CD', 'Client Horizontal': 'CH', 'Stickers': 'SCS', 'AppX': 'APPX',
+    'Native Libraries': 'CH', 'Camera': 'CAM', 'Stories and Timeline': 'STO', 'Profile and Privacy': 'PP',
+    'Onboarding': 'ON', 'Universal Search & Discovery': 'US', 'Backup Restore': 'BR', 'College/Community': 'COM',
+    'Friends': 'FRIENDS', 'Notification': 'NOTIF', '': 'MIS', 'Explore': 'CD', 'Jokes': 'CD','News': 'CD', 'hikemetab': 'CD', 'Wallet': 'HP', 'Hike Run (Beta)': 'HL', 'Bae Buddy Bro': 'HL',
+    'Hike Rewards': 'HL', 'Confessions': 'HL', 'HikeReactStickerShop': 'SCS', 'Gift Packets': 'HP',
+    'Recharge': 'HP', 'Blue Packets': 'HP', 'Polls': 'BCN', 'Vote': 'BCN', 'Bill Split': 'BCN',
+    'Checklist': 'BCN'}
+
+
+
 class MyCrashMonitorTable extends React.Component {
 
     constructor() {
@@ -109,16 +239,6 @@ class MyCrashMonitorTable extends React.Component {
             <TableHeaderColumn headerAlign='center' dataAlign='center' width='250' columnTitle={ true } dataField='firstBuild'>{this.columnNames[13]}</TableHeaderColumn>
             <TableHeaderColumn headerAlign='center' dataAlign='center' width='250' columnTitle={ true } dataField='lastBuild'>{this.columnNames[14]}</TableHeaderColumn>
         </BootstrapTable>
-            // <Table
-            //     items={this.props.data}
-            //     columns={this.columns}
-            //     columnNames={this.columnNames}
-            //     itemsPerPage={this.state.itemsPerPage}
-            //     search={this.state.search}
-            //     onSearch={this.onSearch}
-            //     onChangeItemsPerPage={this.onChangeItemsPerPage}
-            //     width={1000}
-            // />
         )
     }
 }
